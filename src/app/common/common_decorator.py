fc67c75ca680ec63675ec4f4894429ffb24087e8
Wang, Tao <wangtao@cookding.com>
2025-05-19 17:21:18 +0800

    refactor: revise set route metadata decorator

--- src/app/common/common_decorator.py
+++ src/app/common/common_decorator.py
@@ -1,4 +1,5 @@
 from functools import wraps
+from types import MappingProxyType
 from typing import Any, Callable, TypeVar, cast
 
 from app.common.common_schema import JWTAudience
@@ -10,16 +11,17 @@ def _set_route_metadata(
     **metadata: Any,
 ) -> Callable[[DecoratedCallable], DecoratedCallable]:
     def decorator(callable: DecoratedCallable) -> DecoratedCallable:
-        if not hasattr(callable, "metadata"):
-            callable.metadata = {}  # type: ignore[attr-defined]
-        callable.metadata.update(metadata)  # type: ignore[attr-defined]
-
         @wraps(callable)
-        async def wrapped(*args: Any, **kwargs: Any) -> Any:
+        async def wrapper(*args: Any, **kwargs: Any) -> Any:
             return await callable(*args, **kwargs)
 
-        wrapped.metadata = callable.metadata  # type: ignore[attr-defined]
-        return cast(DecoratedCallable, wrapped)
+        wrapper.metadata = MappingProxyType(  # type: ignore[attr-defined]
+            {
+                **getattr(callable, "metadata", {}),
+                **metadata,
+            }
+        )
+        return cast(DecoratedCallable, wrapper)
 
     return decorator
 
